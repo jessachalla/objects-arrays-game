103f729ee363c5e9272b48c72fd7480314356f7e
jessachalla <jessa.challa@gvmc.org>
2019-02-25 18:54:01 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,61 @@
+const blossom = {
+    name: "Blossom",
+    health: 50,
+}
+const bubbles = {
+    name: "Bubbles",
+    health: 50,
+}
+const buttercup = {
+    name: "Buttercup",
+    health: 50,
+}
+const characters = [blossom, bubbles, buttercup];
+
+const opponent = {
+    name: "Mojo Jojo",
+    health: 100,
+}
+
+while (opponent.health >= -1){
+    let start = prompt("Choose which character you want to fight: 1 for Blossom, 2 for Bubbles, or 3 for Buttercup.");
+    if (start === null) {
+        console.log("You have left the game.");
+        break;
+    }
+    else if (opponent.health < 1){
+        console.log("YOU HAVE DEFETED MOJO JOJO!")
+    }
+    else if (start === "1") {
+        if (blossom.health <= 0) {
+            console.log("Sorry pick another character.")
+        }
+        else if (blossom.health >= 0) {
+        blossom.health = blossom.health - 5;
+        opponent.health = opponent.health - 5; 
+            }
+        }
+    else if (start === "2") {
+        if (bubbles.health <= 0) {
+            console.log("Sorry pick another character.")
+        }
+        else if (bubbles.health >= 0) {
+        bubbles.health = bubbles.health - 5;
+        opponent.health = opponent.health - 5;
+        }
+    }
+    else if (start === "3") {
+        if (buttercup.health <= 0) {
+            console.log("Sorry pick another character.")
+        }
+        else if (buttercup.health >= 0) {
+        buttercup.health = buttercup.health - 5;
+        opponent.health = opponent.health - 5;
+        }
+    }
+    console.log(blossom.name, blossom.health);
+    console.log(bubbles.name, bubbles.health);
+    console.log(buttercup.name, buttercup.health);
+    console.log(opponent.name, opponent.health);
+}
+
